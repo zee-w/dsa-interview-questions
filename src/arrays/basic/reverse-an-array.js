020c0d53beac81a361466c7caa74689655445376
zee-w <zeeshan.wani10@gmail.com>
2024-06-29 19:16:41 +0530

    reverse an array

---
+++ src/arrays/basic/reverse-an-array.js
@@ -0,0 +1,11 @@
+function solution(A) {
+    let B=[];
+    let ALength=A.length
+    for (let i = 1; i <= A.length; i++) {
+        B[i-1]=A[ALength-i]
+    }
+    return B
+}
+
+
+console.log(solution([7,8,9,20,23]));
